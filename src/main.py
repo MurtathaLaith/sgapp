ab0152a7d7773cdcaf3175e0ca6cf3a2ac867ff0
Murtatha Laith <45479856+MurtathaLaith@users.noreply.github.com>
2025-06-24 13:38:12 +0300

    Update  fix main.py

--- src/main.py
+++ src/main.py
@@ -47,5 +47,5 @@ def serve(path):
 
 
 if __name__ == '__main__':
-port = int(os.environ.get('PORT', 5001))
-app.run(host='0.0.0.0', port=port, debug=False)
+    port = int(os.environ.get('PORT', 5001))
+    app.run(host='0.0.0.0', port=port, debug=False)
